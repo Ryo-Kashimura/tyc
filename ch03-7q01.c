d5797fff5c9488bf62b3049a3b2f5d75aa2043eb
鹿志村諒 <dqlover2352@akane.waseda.jp>
2019-10-17 09:53:41 +0900

    new file:   ch02-01.c 	new file:   ch02-01.exe 	new file:   ch02-8q03.c 	new file:   ch02-8q03.exe 	new file:   ch03-2q03.c 	new file:   ch03-2q03.exe 	new file:   ch03-5q02.c 	new file:   ch03-5q02.exe 	new file:   ch03-5q03.c 	new file:   ch03-5q03.exe 	new file:   ch03-7q01.c 	new file:   ch03-7q01.exe 	new file:   ch03-8q01.c 	new file:   ch03-8q01.exe 	new file:   ch03-8q02.c 	new file:   ch03-8q02.exe 	new file:   ch03-8q03.c 	new file:   ch03-8q03.exe 	new file:   ch03-8q04.c 	new file:   ch03-8q04.exe 	new file:   ch03-9q01.c 	new file:   ch03-9q01.exe 	new file:   ch03-9q03.c 	new file:   ch03-9q03.exe 	new file:   ch03-9q04.c 	new file:   ch03-9q04.exe

---
+++ ch03-7q01.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 0x00A0;
+    printf("x = %d\n", x);
+    return 0;
+}
---
+++ ch03-9q01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void) {
+    long long value = LLONG_MAX;
+    printf("LLONG_MAX = %lli\n", value);
+    return 0;
+}
